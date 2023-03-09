d6b5ac1906bb768d3f9982e80d0e3e52872ff6d0
Astrium <romain.mellaza@orange.fr>
2023-03-09 22:12:47 +0100

    Create SUJET_45_CORRECTION.py

---
+++ SUJET_45/SUJET_45_CORRECTION.py
@@ -0,0 +1,49 @@
+# SUJET 45
+
+# Exo 1 :
+def rangement_valeurs(tab):
+    res = []
+    for i in range(0,11):
+        counter = 0
+        for j in range(len(tab)):
+            if tab[j] == i :
+                counter += 1
+        res.append(counter)
+    return res
+
+def notes_triees(tab):
+    res = []
+    for i in range(len(tab)):
+        for j in range(tab[i]):
+            res.append(i)
+    return res
+
+# Appels de vérification :
+notes_eval = [2, 0, 5, 9, 6, 9, 10, 5, 7, 9, 9, 5, 0, 9, 6, 5, 4]
+effectifs_notes = rangement_valeurs(notes_eval)
+print(effectifs_notes)
+print(notes_triees(effectifs_notes))
+
+# Exo 2 :
+def dec_to_bin(nb_dec):
+    q, r = nb_dec // 2, nb_dec % 2
+    if q == 0:
+        return str(r)
+    else:
+        return dec_to_bin(q) + str(r)
+
+def bin_to_dec(nb_bin):
+    if nb_bin == '0':
+        return 0
+    elif nb_bin == '1':
+        return 1
+    else:
+        if nb_bin[-1] == '0':
+            bit_droit = 0
+        else:
+            bit_droit = 1
+        return 2 * bin_to_dec(nb_bin[:-1]) + bit_droit
+
+# Appels de vérification :
+print(dec_to_bin(25))
+print(bin_to_dec('101010'))
